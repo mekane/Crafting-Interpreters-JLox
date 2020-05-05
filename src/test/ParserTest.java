1525ae9f9af88c2df83d5302f94bdcc34aeca19e
Marty Kane <marty.kane@gmail.com>
2020-05-04 23:42:39 -0500

    reorder tests to match order of rules in the book

--- src/test/ParserTest.java
+++ src/test/ParserTest.java
@@ -8,58 +8,15 @@ import static org.junit.jupiter.api.Assertions.*;
 class ParserTest {
 
     @Test
-    public void parsesTerminals() {
-        Expr.Literal e = (Expr.Literal) parseSingleExpression("27");
-        assertEquals(27.0, e.value);
-
-        e = (Expr.Literal) parseSingleExpression("\"27\"");
-        assertEquals("27", e.value);
-
-        e = (Expr.Literal) parseSingleExpression("false");
-        assertEquals(false, e.value);
-
-        e = (Expr.Literal) parseSingleExpression("true");
-        assertEquals(true, e.value);
-
-        e = (Expr.Literal) parseSingleExpression("nil");
-        assertEquals(null, e.value);
-    }
-
-    @Test
-    public void parsesExpressionsInParentheses() {
-        Expr.Grouping g = (Expr.Grouping) parseSingleExpression("(19)");
-        Expr.Literal e = (Expr.Literal) g.expression;
-        assertEquals(19.0, e.value);
-    }
-
-    @Test
-    public void parsesUnaryOperators() {
-        Expr.Unary e = (Expr.Unary) parseSingleExpression("-1");
-        Token token = e.operator;
-        Expr.Literal right = (Expr.Literal) e.right;
-
-        assertEquals(TokenType.MINUS, token.type);
-        assertEquals(1.0, right.value);
-
-
-        e = (Expr.Unary) parseSingleExpression("!0");
-        token = e.operator;
-        right = (Expr.Literal) e.right;
-
-        assertEquals(TokenType.BANG, token.type);
-        assertEquals(0.0, right.value);
+    public void parsesNotEqualOperator() {
+        Expr.Binary e = (Expr.Binary) parseSingleExpression("false != true");
+        assertBinaryExpression(e, false, TokenType.BANG_EQUAL, true);
     }
 
     @Test
     public void parsesEqualityOperator() {
         Expr.Binary e = (Expr.Binary) parseSingleExpression("11 == 28");
         assertBinaryExpression(e, 11.0, TokenType.EQUAL_EQUAL, 28.0);
-    }
-
-    @Test
-    public void parsesNotEqualOperator() {
-        Expr.Binary e = (Expr.Binary) parseSingleExpression("false != true");
-        assertBinaryExpression(e, false, TokenType.BANG_EQUAL, true);
     }
 
     @Test
@@ -87,15 +44,21 @@ class ParserTest {
     }
 
     @Test
+    public void parsesSubtractionOperator() {
+        Expr.Binary e = (Expr.Binary) parseSingleExpression("2 - 2");
+        assertBinaryExpression(e, 2.0, TokenType.MINUS, 2.0);
+    }
+
+    @Test
     public void parsesAdditionOperator() {
         Expr.Binary e = (Expr.Binary) parseSingleExpression("2 + 2");
         assertBinaryExpression(e, 2.0, TokenType.PLUS, 2.0);
     }
 
     @Test
-    public void parsesSubtractionOperator() {
-        Expr.Binary e = (Expr.Binary) parseSingleExpression("2 - 2");
-        assertBinaryExpression(e, 2.0, TokenType.MINUS, 2.0);
+    public void parsesDivisionOperator() {
+        Expr.Binary e = (Expr.Binary) parseSingleExpression("100 / 20");
+        assertBinaryExpression(e, 100.0, TokenType.SLASH, 20.0);
     }
 
     @Test
@@ -105,9 +68,46 @@ class ParserTest {
     }
 
     @Test
-    public void parsesDivisionOperator() {
-        Expr.Binary e = (Expr.Binary) parseSingleExpression("100 / 20");
-        assertBinaryExpression(e, 100.0, TokenType.SLASH, 20.0);
+    public void parsesUnaryOperators() {
+        Expr.Unary e = (Expr.Unary) parseSingleExpression("-1");
+        Token token = e.operator;
+        Expr.Literal right = (Expr.Literal) e.right;
+
+        assertEquals(TokenType.MINUS, token.type);
+        assertEquals(1.0, right.value);
+
+
+        e = (Expr.Unary) parseSingleExpression("!0");
+        token = e.operator;
+        right = (Expr.Literal) e.right;
+
+        assertEquals(TokenType.BANG, token.type);
+        assertEquals(0.0, right.value);
+    }
+
+    @Test
+    public void parsesTerminals() {
+        Expr.Literal e = (Expr.Literal) parseSingleExpression("27");
+        assertEquals(27.0, e.value);
+
+        e = (Expr.Literal) parseSingleExpression("\"27\"");
+        assertEquals("27", e.value);
+
+        e = (Expr.Literal) parseSingleExpression("false");
+        assertEquals(false, e.value);
+
+        e = (Expr.Literal) parseSingleExpression("true");
+        assertEquals(true, e.value);
+
+        e = (Expr.Literal) parseSingleExpression("nil");
+        assertEquals(null, e.value);
+    }
+
+    @Test
+    public void parsesExpressionsInParentheses() {
+        Expr.Grouping g = (Expr.Grouping) parseSingleExpression("(19)");
+        Expr.Literal e = (Expr.Literal) g.expression;
+        assertEquals(19.0, e.value);
     }
 
     private Expr parseSingleExpression(String expression) {
