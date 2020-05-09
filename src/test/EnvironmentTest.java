02c3bc95becd4a67ece3015c748c3a5ec735a9ee
Marty Kane <marty.kane@gmail.com>
2020-05-08 21:12:22 -0500

    Add unit tests for Environments

---
+++ src/test/EnvironmentTest.java
@@ -0,0 +1,66 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.craftinginterpreters.lox.*;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EnvironmentTest {
+    static Token variable = new Token(TokenType.IDENTIFIER, "name", null, 0);
+    static Token undefined = new Token(TokenType.IDENTIFIER, "undefined", null, 0);
+
+    @Test
+    public void itSupportSettingAndSettingValues() {
+        Environment e = new Environment();
+        e.define(variable.lexeme, "value");
+
+        assertEquals("value", e.get(variable));
+    }
+
+    @Test
+    public void itThrowsRuntimeErrorWhenAccessingUndefined() {
+        Environment e = new Environment();
+        RuntimeError error = assertThrows(RuntimeError.class, () -> e.get(undefined));
+        assertTrue(error.getMessage().contains("Undefined variable"), "Got " + error.getMessage());
+    }
+
+    @Test
+    public void itSupportsReAssigningValuesThatHaveBeenSet() {
+        Environment e = new Environment();
+        e.define(variable.lexeme, "value");
+        e.assign(variable, "newValue");
+
+        assertEquals("newValue", e.get(variable));
+    }
+
+    @Test
+    void itThrowsRuntimeErrorWhenAssigningUndefined() {
+        Environment e = new Environment();
+        RuntimeError error = assertThrows(RuntimeError.class, () -> e.assign(undefined, true));
+        assertTrue(error.getMessage().contains("Undefined variable"), "Got " + error.getMessage());
+    }
+
+    @Test
+    public void itSupportsGettingValuesFromParentEnvironment() {
+        Environment parent = new Environment();
+        parent.define(variable.lexeme, "value");
+
+        Environment e = new Environment(parent);
+        assertEquals("value", e.get(variable));
+    }
+
+    @Test
+    public void itSupportsReAssigningValuesThatHaveBeenSetOnParent() {
+        Environment parent = new Environment();
+        parent.define(variable.lexeme, "value");
+
+        Environment e = new Environment(parent);
+        e.assign(variable, "newValue");
+
+        assertEquals("newValue", e.get(variable));
+        assertEquals("newValue", parent.get(variable));
+    }
+}
